047e8ff09752c91c8fe8b0b06aed70d0d5ab9af9
mohamed laadjal <laadjalmohamed@gmail.com>
2024-12-31 09:43:22 +0100

    feat: Add users entity with initial configuration

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column({ type: 'simple-array', default: ['user'] })
+  roles: string[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
