b834b5754d238b3198805002c69e9d62e910dfe1
mohamed laadjal <laadjalmohamed@gmail.com>
2025-01-01 20:26:12 +0100

    feat(users-validation): add validation constraints for user creation
    Implemented validation rules to ensure proper data integrity during user creation:
    - Added constraints for email, password, and other fields in CreateUserDto.
    - Improved error handling for invalid user input.
    - Updated the UsersService to validate input data before saving a new user.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomValidationPipe } from './common/pipes/validation.pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new CustomValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -14,30 +14,33 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('users')
-@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
+  @Post() // Route d'inscription non protégée
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @UseGuards(JwtAuthGuard) // Protection uniquement pour les routes GET, PATCH, DELETE
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
